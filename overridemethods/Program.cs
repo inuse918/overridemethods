69575539255e703a80bade3d7df029db991f9824
chaerin918 <s2018s34@e-mirim.hs.kr>
2020-06-04 14:54:02 +0900

    계산 메소드 추가

--- overridemethods/Program.cs
+++ overridemethods/Program.cs
@@ -8,8 +8,45 @@ namespace overridemethods
 {
     class Program
     {
+        class Cal
+        {
+            public int Power(int input)
+            {
+                return (int)(Math.Pow(input, 2));
+            }
+            public int Power(int input, int count)
+            {
+                int result = (int)(Math.Pow(input, count));
+                return result;
+            }
+            public int SumAll(int end)
+            {
+                int sum = 0;
+                for (int i = 0; i <= end; i++)
+                {
+                    sum += i;
+                }
+                return sum;
+            }
+            public int SumAll(int start, int end)
+            {
+                int sum = 0;
+                for (int i = start; i <= end; i++)
+                {
+                    sum += i;
+                }
+                return sum;
+            }
+        }
+        
         static void Main(string[] args)
         {
+            Cal cal = new Cal();
+            
+            Console.WriteLine(cal.Power(5));
+            Console.WriteLine(cal.Power(5,3));
+            Console.WriteLine(cal.SumAll(3));
+            Console.WriteLine(cal.SumAll(1,3));
         }
     }
 }
